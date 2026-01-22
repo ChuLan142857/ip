4791d2c6911566bb7f6789cc21284590c7837116
Zhou Jinhao <e1481834@u.nus.edu>
2026-01-22 20:35:58 +0800

    rename main class and add greeting

---
+++ src/main/java/Rei.java
@@ -0,0 +1,14 @@
+public class Rei {
+    public static void main(String[] args) {
+        String horizontal_line = "____________________________________________________________\n";
+        /*
+        String logo = " ____        _        \n"
+                + "|  _ \\ _   _| | _____ \n"
+                + "| | | | | | | |/ / _ \\\n"
+                + "| |_| | |_| |   <  __/\n"
+                + "|____/ \\__,_|_|\\_\\___|\n";
+         */
+        System.out.println(horizontal_line + "Hello! I'm Rei.\n" + "What can I do for you?\n"
+            +horizontal_line + "Bye. Hope to see you again soon!\n" + horizontal_line);
+    }
+}
