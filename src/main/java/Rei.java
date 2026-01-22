43f45f4b82e718c9f054706cdfbb226f4317b9dc
Zhou Jinhao <e1481834@u.nus.edu>
2026-01-22 21:28:46 +0800

    introduce mark/unmark feature without Task

--- src/main/java/Rei.java
+++ src/main/java/Rei.java
@@ -10,6 +10,7 @@ public class Rei {
 
         Scanner scanner = new Scanner(System.in);
         ArrayList<String> items = new ArrayList<>();
+        ArrayList<Boolean> isDone = new ArrayList<>();
 
         while(true){
             String userInput = scanner.nextLine();
@@ -21,14 +22,40 @@ public class Rei {
 
             if(userInput.equals("list")){
                 System.out.println(horizontal_line);
+                System.out.println("Here are the tasks in your list:\n");
                 for (int i = 0; i < items.size(); i++){
-                    System.out.println((i + 1) + ". " + items.get(i));
+                    String status = isDone.get(i) ? "[X]" : "[]";
+                    System.out.println((i + 1) + ". " + status + " " + items.get(i));
                 }
                 System.out.println(horizontal_line);
                 continue;
             }
 
+            if(userInput.startsWith("mark ")){
+                int index = Integer.parseInt(userInput.substring(5)) - 1;
+                isDone.set(index, true);
+
+                System.out.println(horizontal_line);
+                System.out.println("Nice! I've marked this task as done:\n");
+                System.out.println("[X] " + items.get(index));
+                System.out.println(horizontal_line);
+                continue;
+            }
+
+            if(userInput.startsWith("unmark ")){
+                int index = Integer.parseInt(userInput.substring(7)) - 1;
+                isDone.set(index, false);
+
+                System.out.println(horizontal_line);
+                System.out.println("Ok, I've marked this task as not done yet:\n");
+                System.out.println("[ ] " + items.get(index));
+                System.out.println(horizontal_line);
+                continue;
+            }
+
             items.add(userInput);
+            isDone.add(false);
+
             System.out.println(horizontal_line);
             System.out.println("added: " + userInput);
             System.out.println(horizontal_line);
