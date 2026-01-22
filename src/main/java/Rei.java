7ae32aee9ad1ed53ce8034d6338b6c5b6c12be19
Zhou Jinhao <e1481834@u.nus.edu>
2026-01-22 20:52:30 +0800

    Add echo

--- src/main/java/Rei.java
+++ src/main/java/Rei.java
@@ -1,14 +1,25 @@
+import java.util.Scanner;
+
 public class Rei {
     public static void main(String[] args) {
         String horizontal_line = "____________________________________________________________\n";
-        /*
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-         */
+
         System.out.println(horizontal_line + "Hello! I'm Rei.\n" + "What can I do for you?\n"
-            +horizontal_line + "Bye. Hope to see you again soon!\n" + horizontal_line);
+            +horizontal_line);
+
+        Scanner scanner = new Scanner(System.in);
+
+        while(true){
+            String userInput = scanner.nextLine();
+
+            if(userInput.equals("bye")) {
+                System.out.println(horizontal_line+ "Bye. Hope to see you again soon!\n" + horizontal_line);
+                break;
+            }
+
+            System.out.println(horizontal_line + userInput + "\n" + horizontal_line);
+        }
+
+        scanner.close();
     }
 }
